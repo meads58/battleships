32688e02043039485ea5ba2e44b1a244062b602b
meads58 <meads109@gmail.com>
2015-02-11 15:21:59 +0000

    refactor of the board to create ships

--- lib/board.rb
+++ lib/board.rb
@@ -4,16 +4,32 @@ class Board
 
   def initialize
     @grid = {a1: "water", a2: "water", b1: "water", b2: "water"}
+    @ship1 = Ship.new
   end
 
   def place_ship(position)
-    @grid[position] = "ship"
+    @grid[position] = @ship1
   end
 
   def check_shot(position)
-    if @grid[position] == "ship"
-      return "Hit!"
+    if @grid[position] == @ship1
+      "Hit!"
+      speak_to_ship
+        else
+      "Miss"
     end
+  end
+
+  def unsunk_ship_count
+    count = 0
+    @grid.keys.each do |cell|
+      count += 1 if grid[cell] == @ship1
+    end
+    count
+  end
+
+  def speak_to_ship
+    @ship1.damage_from_hit
   end
 
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -3,6 +3,7 @@ require 'board'
 describe Board do
   
   let(:board) { Board.new }
+  let(:ship) {double :ship}
 
   it 'has an initial size of 2*2' do
     expect(board.grid.keys).to eq [:a1, :a2, :b1,:b2] 
@@ -10,12 +11,27 @@ describe Board do
 
   it 'can place a ship' do
     board.place_ship(:a1)
-    expect(board.grid.values).to eq ["ship", "water", "water", "water"]
+    expect(board.grid.values).eq [ship, "water", "water", "water"]
   end
 
-  it 'can check a shot' do
+  it 'return hit if shot hits ship' do
     board.place_ship(:a1)
     expect(board.check_shot(:a1)).to eq "Hit!"
+  end
+
+  it 'should tell ship it has been hit when hit' do
+    board.place_ship(:a1)
+    expect(ship).to receive(:damage_from_hit)
+    board.speak_to_ship
+  end
+
+  it 'return miss if shots hit water' do
+    expect(board.check_shot(:a1)).to eq "Miss"
+  end
+
+  it 'knows how many ships of size 1 are on the grid' do
+    board.place_ship(:a1)
+    expect(board.unsunk_ship_count).to eq 1
   end
 
 
