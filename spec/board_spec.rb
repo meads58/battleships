f4772d3c740e760c30617f552d015e4ca6f203a3
Paul Fitzgerald <paulfitz99@gmail.com>
2015-02-12 09:54:02 +0000

    fixed rspec tests

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -3,8 +3,8 @@ require 'board'
 describe Board do
   
   let(:board) { Board.new }
-  let(:ship) { double :ship, hits_left: 1 }
-  let(:submarine) { double :ship, hits_left: 2 }
+  let(:ship) { double :ship }
+  let(:submarine) { double :ship}
 
   it 'has an initial size of 2*2' do
     expect(board.grid.keys).to eq [:a1, :a2, :b1,:b2] 
@@ -17,7 +17,7 @@ describe Board do
   end
 
   it 'can put a submarine on the board' do
-      allow(ship).to receive(:hits_left) 
+      allow(submarine).to receive(:hits_left).and_return 2
       board.place_ship(:a1, submarine)
       expect(board.grid.values).to eq [submarine, "water", submarine, "water"]
   end
@@ -30,7 +30,7 @@ describe Board do
   end
 
   it 'should tell ship it has been hit when hit' do
-    expect(ship).to receive(:hits_left)
+    allow(ship).to receive(:hits_left).and_return 1
     board.place_ship(:a1, ship)
     expect(ship).to receive(:damage_from_hit)
     board.speak_to_ship(ship)
